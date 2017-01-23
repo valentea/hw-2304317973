7c9c986a50a3b9bbb8e94bac0e114a23b7e7d726
valentea <valentea@gmail.com>
2017-01-23 09:41:51 -0800

    Homework One Submittion (not final)

---
+++ hw1/wsearch/cmake-build-debug/CMakeFiles/wsearch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wsearch.dir/wsearch.cpp.o"
+  "wsearch.pdb"
+  "wsearch.exe"
+  "libwsearch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wsearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
