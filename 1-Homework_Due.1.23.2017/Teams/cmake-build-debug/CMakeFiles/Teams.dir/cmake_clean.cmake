8c95cbef3bae6b8ba7362e46a8c706e8ed13f120
valentea <valentea@gmail.com>
2017-01-23 09:59:28 -0800

    Homework 1

---
+++ 1-Homework_Due.1.23.2017/Teams/cmake-build-debug/CMakeFiles/Teams.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Teams.dir/main.cpp.o"
+  "Teams.pdb"
+  "Teams.exe"
+  "libTeams.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Teams.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1-Homework_Due.1.23.2017/WordSearch/cmake-build-debug/CMakeFiles/WordSearch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WordSearch.dir/main.cpp.o"
+  "WordSearch.pdb"
+  "WordSearch.exe"
+  "libWordSearch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WordSearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
