658f71a75fb28af58ff62cd3c267feaa1aa48bdc
valentea <valentea@usc.edu>
2017-01-23 23:26:38 -0800

    Code for easy access and editing across platforms

---
+++ cLionWork (DO NOT GRADE)/1-Homework_Due.1.23.2017/Teams/cmake-build-debug/CMakeFiles/Teams.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Teams.dir/teams.cpp.o"
+  "Teams.pdb"
+  "Teams.exe"
+  "libTeams.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Teams.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
