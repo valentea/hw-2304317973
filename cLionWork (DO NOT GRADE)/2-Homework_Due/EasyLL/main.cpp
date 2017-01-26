103be19b2727debb63a142176157cfe3168a8ab5
valentea <valentea@usc.edu>
2017-01-26 10:54:56 -0800

    Homework2 Start

---
+++ cLionWork (DO NOT GRADE)/2-Homework_Due/EasyLL/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+struct Item{
+Item(int v, Item* n) { val = v; next = n;}
+    int val;
+    Item* next;
+};
+
+Item* concatenate(Item* head1, Item* head2);
+void removeEvens(Item*& head);
+double findAverage(Item* head);
+
+Item *concatenate(Item* head1, Item* head2){
+    Item* temp;
+    for()
+}
+
+
+
+
+int main(int argc, char* argv[]) {
+
+}
