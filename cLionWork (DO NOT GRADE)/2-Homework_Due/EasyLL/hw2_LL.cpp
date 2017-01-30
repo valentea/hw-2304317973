b60e4ddc86aa1bc2e7ece5d57bcd10a0638d5147
valentea <valentea@usc.edu>
2017-01-29 22:32:46 -0800

    Homework Two Start

--- cLionWork (DO NOT GRADE)/2-Homework_Due/EasyLL/hw2_LL.cpp
+++ cLionWork (DO NOT GRADE)/2-Homework_Due/EasyLL/hw2_LL.cpp
@@ -14,15 +14,15 @@ void removeEvens(Item*& head);
 double findAverage(Item* head);
 void constructList(Item* &head, int val);
 
-//Item *concatenate(Item* head1, Item* head2){
-//    Item* temp;
-//    if(head1 != NULL){
-//        concatenate(head1->next, head2);
-//    } else{
-//        head2 = head1;
-//        return head2;
-//    }
-//}
+Item* concatenate(Item* head1, Item* head2) {
+    if (head1 != NULL){
+        return concatenate(head1->next, head2);
+    }
+    else{
+        head1 = head2;
+        return head1;
+    }
+}
 
 void constructList(Item* &head, int val){
     Item* newHead = new Item;
@@ -45,32 +45,49 @@ void constructList(Item* &head, int val){
 
 
 int main(int argc, char* argv[]) {
-    if(argc > 2){
+    if (argc > 2) {
         cerr << "Only need input.in" << endl;
         return 1;
     }
 
     ifstream ifile(argv[1]);
-    if(ifile.fail()){
+    if (ifile.fail()) {
         cerr << "Error" << endl;
         return 1;
     }
 
-    Item* headListOne = NULL;
-    Item* headListTwo = NULL;
+    Item *headListOne = NULL;
+    Item *headListTwo = NULL;
     string temp;
-    while(getline(ifile,temp)) {
-        int addNumber;
-        stringstream listNumbers(temp);
-        if(!(listNumbers >> addNumber).fail()) {
-            constructList(headListOne, addNumber);
-            cout << headListOne->val << endl;
-            headListOne = headListOne->next;
-        } else{ break;}
+    //MAKE ORIGINAL TWO LL
+    getline(ifile, temp);
+    int addNumber;
+    stringstream numberListOne(temp);
+    cout << "list One" << endl;
+    while (numberListOne >> addNumber){
+        constructList(headListOne, addNumber);
+        cout << headListOne->val << endl;
+        headListOne = headListOne->next;
+    }
+    getline(ifile, temp);
+    cout << "list Two" << endl;
+    stringstream numberListTwo(temp);
+    while (numberListTwo >> addNumber){
+        constructList(headListTwo, addNumber);
+        cout << headListTwo->val << endl;
+        headListTwo = headListTwo->next;
     }
 
-    return 7;
+    //MAKE ONE BIG LIST
+    Item* bigList = concatenate(headListOne, headListTwo);
+    cout << "BigList" << endl;
+    while(bigList != NULL){
+        cout << bigList->val << endl;
+        bigList = bigList->next;
+    }
+    cout << "end of BigList" << endl;
 
+    return 7;
 
 
 }
