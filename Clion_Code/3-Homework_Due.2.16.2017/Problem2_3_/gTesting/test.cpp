fa7280c30dffe2781bc9af6f0b900f3e856150da
valentea <valentea@gmail.com>
2017-02-14 22:23:29 -0800

    working on number 3

--- Clion_Code/3-Homework_Due.2.16.2017/Problem2_3_/gTesting/test.cpp
+++ Clion_Code/3-Homework_Due.2.16.2017/Problem2_3_/gTesting/test.cpp
@@ -19,13 +19,62 @@ protected:
 	}
 	
 	virtual void TearDown() {
-	   myList.clear();
+	   //myList.clear();
 	}
 
 	LListInt myList;
 };
 
-TEST_F(LListIntTest, SizeTest) {
-	// Exepct 0th element to be 1, 1st elemnt to be 2, etc.
-   EXPECT_EQ(10, myList.size());
+TEST_F(LListIntTest, CopyConstructorTestSize) {
+   LListInt test(myList);
+   ASSERT_TRUE(test.size() == myList.size());
 }
+   
+TEST_F(LListIntTest, CopyConstructorTestVals){
+   LListInt test(myList);
+   for(int i = 0; i < test.size(); i++){
+   	EXPECT_TRUE(test.get(i) == myList.get(i));
+   }
+}   
+
+TEST_F(LListIntTest, CopyConstructorTestEmptyList){
+   LListInt test(myList);
+   myList.clear();
+   LListInt test1(myList);
+   ASSERT_TRUE(test1.size() == myList.size());
+}
+
+TEST_F(LListIntTest, AssignmentOperatorTestSize) {
+   LListInt test;
+   test = myList;
+   ASSERT_TRUE(test.size() == myList.size());
+}
+   
+TEST_F(LListIntTest, AssignmentOperatorTestVals){
+   LListInt test;
+   test = myList;
+   for(int i = 0; i < test.size(); i++){
+   	EXPECT_TRUE(test.get(i) == myList.get(i));
+   }
+}   
+
+TEST_F(LListIntTest, AssignmentOperatorTestEmptyList){
+   LListInt test;
+   test = myList;
+   myList.clear();
+   LListInt test1(myList);
+   ASSERT_TRUE(test1.size() == myList.size());
+}
+
+TEST_F(LListIntTest, PushBackBasic){
+	myList.push_back(10);
+	EXPECT_EQ(myList.get(myList.size()-1), 10);
+}
+
+TEST_F(LListIntTest, PushBackEmpty){
+   myList.clear();
+	myList.push_back(10);
+	EXPECT_EQ(myList.get(myList.size()), 10);
+}
+
+
