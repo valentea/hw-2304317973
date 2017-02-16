9dfad1d48de4ec59be9f987b4ee5e745d3490683
valentea <valentea@usc.edu>
2017-02-16 02:46:18 -0800

    makeFile build for VM

---
+++ Clion_Code/3-Homework_Due.2.16.2017/Problem5/llistint_test.cpp
@@ -0,0 +1,35 @@
+#include "llistint.h"
+#include <iostream>
+
+using namespace std;
+
+ostream& operator<<(ostream &os, const LListInt& list) {
+    for (int i = 0; i < list.size(); ++i) {
+        os << list.get(i) << " ";
+        if ((i+1) % 10 == 0) {
+            cout << endl;
+        }
+    }
+    return os;
+}
+
+
+int main() {
+
+    LListInt list;
+
+    if(list.empty()){
+        cout << "Passed Empty Intialize test" << endl;
+    }
+
+    list.insert(0, 1);
+    list.insert(1, 2);
+    list.insert(1, 3);
+
+    LListInt list2;
+
+    list += list2;
+
+
+    return 1;
+}
