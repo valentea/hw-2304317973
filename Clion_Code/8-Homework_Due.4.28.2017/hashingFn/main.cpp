9e2d8d21924a6ebc585fbfdce0a1a34252edd0a8
valentea <valentea@usc.edu>
2017-04-25 23:50:02 -0700

    hashing fn start

--- Clion_Code/8-Homework_Due.4.28.2017/hashingFn/main.cpp
+++ Clion_Code/8-Homework_Due.4.28.2017/hashingFn/main.cpp
@@ -13,22 +13,37 @@ int main() {
     unsigned long long hash = 0;
     array<int, 4> final;
 
+    cout << "single letters" << endl;
     for (int i = 0; i < password.length(); ++i) {
         singleLetters.push_back(password[i]);
         cout << singleLetters[i] << " ";
     }
-    cout << endl;
+    cout << endl << endl;
 
-    for (int j = (int)singleLetters.size(); j > 0; j--) {
+    cout << "single letter 'int' conversion" << endl;
+    for (int m = 0; m < singleLetters.size(); ++m) {
+        cout << (int)singleLetters[m] << " ";
+    }
+    cout << endl << endl;
+
+
+    cout << "single letter 'int' conversion IN REVERSE" << endl;
+    for (int j = (int)singleLetters.size()-1; j >= 0; j--) {
+        cout << (int)singleLetters[j] << " ";
         numbers.push_back(pow(128,(singleLetters.size() - j)) * (int)singleLetters[j]);
     }
+    cout << endl << endl;
+
+    cout << "numbers: " << endl;
     for (int l = 0; l < 8; ++l) {
         cout << l << ": " << numbers[l] << endl;
     }
     cout << endl;
 
+
+    cout << "hash before encryption: " << endl;
     for (int k = 0; k < singleLetters.size(); ++k) {
-        hash = hash + singleLetters[k];
+        hash = hash + (int)singleLetters[k];
     }
 
     cout << hash << endl;
